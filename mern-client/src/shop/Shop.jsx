2ee0763002a904119716459f4d441fde08ea803e
yehmight <145411012+yehmight@users.noreply.github.com>
2024-11-07 01:47:00 +0100

    Update Shop.jsx

--- mern-client/src/shop/Shop.jsx
+++ mern-client/src/shop/Shop.jsx
@@ -6,7 +6,7 @@ const Shop = () => {
   const [books, setBooks] =useState([]);
 
   useEffect (() => {
-    fetch("http://localhost:5000/all.books").then(res => res.json()).then(data => setBooks(data));
+    fetch("https://book-store-inventory.onrender.com/all.books").then(res => res.json()).then(data => setBooks(data));
   },[])
   return (
     <div className='mt-28 px-4 lg:px24'>
