c901776df35f67e91793a573baf1eaea7cd5d667
yehmight <145411012+yehmight@users.noreply.github.com>
2024-11-07 01:41:50 +0100

    Update BestSellerBooks.jsx

--- mern-client/src/home/BestSellerBooks.jsx
+++ mern-client/src/home/BestSellerBooks.jsx
@@ -5,7 +5,7 @@ const BestSellerBooks = () => {
     const [books, setBooks]= useState ([]);
 
     useEffect( () => {
-        fetch("http://localhost:5000/all.books").then(res => res.json()).then(data => setBooks(data.slice(0,8)))
+        fetch("https://book-store-inventory.onrender.com/all.books").then(res => res.json()).then(data => setBooks(data.slice(0,8)))
     }, [])
   return (
     <div>
